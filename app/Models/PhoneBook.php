cf330eae8db817a4a5521135aee02f356f268992
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2023-12-20 15:38:48 +0800

    first commit

---
+++ app/Models/PhoneBook.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PhoneBook extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'contact_no'
+    ];
+}
